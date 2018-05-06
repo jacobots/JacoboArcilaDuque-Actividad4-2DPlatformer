7feabb3250640d0f1a328cdb6448db13aeff0d26
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-05-05 21:23:13 -0500

    Primera versión

---
+++ JacoboArcila-2DPlatformer/Assets/Scripts/Camara.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour {
+
+	public Transform player;
+	public float offsetX;
+	public float offsetY;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3 (player.transform.position.x + offsetX, offsetY, -10);
+		
+		
+	}
+}
---
+++ JacoboArcila-2DPlatformer/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterControl : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	int coins = 0;
+
+	int hearts = 0;
+
+	int stars = 0;
+
+	public Text contadorCoins;
+
+	public Text contadorHearts;
+
+	public Text contadorStars;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+
+	}
+
+	public void clickEnElBoton(){
+		print ("Botón salto presionado");
+		this.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 7, ForceMode2D.Impulse);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// movimiento hacia la derecha
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.gameObject.transform.Translate (speed, 0f, 0f);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.gameObject.transform.Translate (-speed, 0f, 0f);
+		}
+		// salto
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 7, ForceMode2D.Impulse);
+
+		}
+
+
+
+		
+	}
+
+	// Collision 
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Coins"){
+			
+
+
+			// Aumentar la cantidad de monedas del jugador. 
+
+			coins = coins + 1;
+
+			// Actualizar el componente texto.
+
+			contadorCoins.text = coins.ToString ();
+
+			GameObject.Destroy (coll.gameObject);
+
+
+
+		}
+
+		if (coll.gameObject.tag == "Hearts"){
+
+
+
+			// Aumentar la cantidad de monedas del jugador. 
+
+			hearts = hearts + 1;
+
+			// Actualizar el componente texto.
+
+			contadorHearts.text = hearts.ToString ();
+
+			GameObject.Destroy (coll.gameObject);
+
+
+
+		}
+
+		if (coll.gameObject.tag == "Stars"){
+
+
+
+			// Aumentar la cantidad de monedas del jugador. 
+
+			stars = stars + 1;
+
+			// Actualizar el componente texto.
+
+			contadorStars.text = stars.ToString ();
+
+			GameObject.Destroy (coll.gameObject);
+
+
+
+		}
+
+		if (coll.gameObject.tag == "Enemies"){
+
+
+
+			// Aumentar la cantidad de monedas del jugador. 
+
+			hearts = hearts - 1;
+
+			// Actualizar el componente texto.
+
+			contadorHearts.text = hearts.ToString ();
+
+			GameObject.Destroy (coll.gameObject);
+
+
+
+		}
+	}
+
+}
+
+	
+
